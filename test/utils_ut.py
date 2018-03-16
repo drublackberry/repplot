b0ce1e2b861815178a1bb400ea641d433284a8e6
drublackberry <andreu.mora@gmail.com>
2018-03-16 18:34:36 +0100

    Added first unit tests

--- test/utils_ut.py
+++ test/utils_ut.py
@@ -1,25 +1,23 @@
 import unittest
-import utils
+import reportlib as rep
 import numpy as np
 import pandas as pd
+
+df = pd.DataFrame(columns=['bacon', 'spam', 'beans', 'ham'],
+                  index=pd.date_range(start='2015-01-01', end='2018-01-01'))
+t = np.array(range(len(df)))
+for c in df.columns:
+    df.loc[:, c] = np.random.random(1) * np.sin(2 * np.pi * t / len(df)) + np.random.random(len(df))
 
 
 class TestUtils(unittest.TestCase):
 
-    def test_cluster(self):
+    def test_plot_cat_matplotlib(self):
+        rep.plot_cat(df, smooth_period=7, plot_std=True, plot_original=False, title='SPAM plot', marker=None)
+        rep.plot_cat(df.iloc[-30:], plot_original=False, title='SPAM plot zoomed',
+                     marker='*', show_weekday_on='bacon')
+        self.assertTrue(True)  # assess it has finished
 
-
-
-        self.assertTrue('cluster' in df_out.index.names)
-        self.assertTrue(cluster.time_label in df_out.index.names)
-
-        df_out.loc[:, 'ecpc'] = df_out.revenue / df_out.volume
-        df_orig = cluster.transform(df_out.ecpc)
-
-        self.assertEqual((df.xs('Booking.com', axis=0, level='agency').index == df_orig.index).sum(), 1)
-
-
-
-
-
-
+    def test_plot_fft_matplotlib(self):
+        rep.plot_cat_fft(df[['bacon']])
+        self.assertTrue(True)  # assess it has finished
